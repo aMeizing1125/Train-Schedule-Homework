29ade2e7276395f1f20c83ec232835fc87fd1514
aMeizing1125 <hampton.muriel@gmail.com>
2019-03-11 05:02:15 -0400

    touch ups while rewatching the video. more procrastinating CSS... main reason I am good at it... LOL.. huff back to the video then.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,38 +11,90 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$('#submit').on("click", function(event){
-  event.prevenDefault();//prevents hitting Enter and it going through. 
+$('#submit').on("click", function (event) {
+  event.preventDefault(); //prevents hitting Enter and it going through. 
 
   var newTrain = $('#newTrain').val().trim();
   var newDestination = $('#newDestination').val().trim();
   var newFirstArrival = $('#firstArrival').val().trim();
-  var newFrenquency = $('#newFrequency').val().trim();
-  database.ref().push({
-    newTrain: newTrain,
-    newDestination: newDestination,
-    newFirstArrival: newFirstArrival,
-    newFrequency: newFrequency
+  var newFrequency = $('#newFrequency').val().trim();
+  //help with tutor Number.isInteger()
 
-  })
+  var workingArrival = parseInt(newFirstArrival.replace(":", "")); //it returns it as a string"" and we need to change to a number so boolean can verify
+  var workingFrequency = parseInt(newFrequency);
+  var hasData = (newTrain.length && newDestination.length && newFirstArrival.length && newFrequency.length) ? true : false;
+  var isNumber = (Number.isInteger(workingArrival) && Number.isInteger(workingFrequency)) ? true : false; //replace() javascript method on string - tutor time :)
+  var numberLengthArrival = (workingArrival <= 2400); // this is the max military time 
+  var numberLengthFrequency = (workingFrequency <= 1440); // this is a max frequency you can have in 24 hrs. 
+  if (hasData && isNumber && numberLengthArrival && numberLengthFrequency) { //verifying before pushing to database
+    database.ref().push({
+      newTrain: newTrain,
+      newDestination: newDestination,
+      newFirstArrival: newFirstArrival,
+      newFrequency: newFrequency
 
-  $('.trainName').val("");
-  $('.trainDestination').val("");
-  $('.trainFrequency').val("");
-// $('.nextArrival').val("");
-// $('.minutesAway').val("");
+    })
+
+    $('#newTrain').val("");
+    $('#newDestination').val("");
+    $('#firstArrival').val("");
+    $('#newFrequency').val("");
+  } else {
+    alert("silly rabbit ... fill it out correctly.")
+  }
+
 })
 
-database.ref().on('value', function(snapshot){
-  $('#addTrainsHere').empty();
-  snapshot.forEach(function(thisTrain){
-    console.log(thisTrain);
-    var tRow = $('<tr>');
-    // var tTrainName = $('<td>').text(thisTrain.val().newTrain);
-    // var tDestination = $('<td>').text(thisEmployee.val().newDestination);
-    // var t = $('<td>').text(thisEmployee.val().newFreq);
-    // var tFrequency = $('<td>').text(thisEmployee.val().newFrequestion);
-    tRow.append()
+database.ref().on('value', function (snapshot) {
+  $('#addHere').empty();
+
+  snapshot.forEach(function (thisTrain) {
+    // console.log(thisTrain);
+    // var tRow = $('<tr>');
+    //define variable from firebase
+    var tTrainName = thisTrain.val().newTrain;
+    var tDestination = thisTrain.val().newDestination;
+    var tFirstArrival = thisTrain.val().newFirstArrival;
+    var tFrequency = thisTrain.val().newFrequency;
+    var t = "word";
+    var trainTime;
+    var nextArrival;
+    var minutesRemaining;
+    var hrs;
+    var mins;
+    var timeArray;
+    var timeCheck;
+  
+    //manipulate time vairables
+    timeArray = tFirstArrival.split(":");
+    hrs = timeArray[0];
+    mins = timeArray[1];
+    trainTime = moment().hours(hrs).minutes(mins);
+    timeCheck = moment().max(moment(), trainTime);// store which is greater. 
+    console.log(timeCheck);
+    //compare first arrival to current time
+    if(trainTime === timeCheck) {
+      // what this means is the timeCheck, which is the greater of the 2
+      //current time and trainTime,  is equal to the traintime meaning it is the next arrival time. 
+      //it means the next arrival time is the first arrival time
+      nextArrival = trainTime.format("hh:mm A");// hh:mm A is just the format how we want it to return
+      minutesRemaining = trainTime.diff(moment(), "minutes") //want to return it in minutes which is a string
+
+      
+    }
+    //do calculation to find next train if first train has already come
+
+
+    //append to page
+    // replace the last 2 <td>${t}</td>
+    var html = `<tr>
+  
+      <td>${tTrainName}</td>
+      <td>${tDestination}</td>
+      <td>${tFrequency}</td>
+      <td>${nextArrival}</td>
+      <td>${minutesRemaining}</td> 
+      </tr>`
+    $('#addHere').append(html)
   })
 })
-
